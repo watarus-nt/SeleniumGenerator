bae176d2552d04383de2dc0ce20699af695ee07d
Hung Tran Manh <tranmanhhung@tma.com.vn>
2017-09-01 08:05:26 +0700

    adding executable jar and bat files

--- run.bat
+++ run.bat
@@ -1,12 +1,15 @@
+@echo off
 SET "seleniumProjectPath=D:\Books\SourceCode\SeleniumBase\SeleniumPractices"
 
 java -jar SeleniumGenerator.jar %seleniumProjectPath% sampleAItest.json
 
 cd %seleniumProjectPath%
 
-mvn clean verify -DtestSuiteXML=ai.xml > debug.txt
+call mvn clean verify -DtestSuiteXML=ai.xml 
 
 echo Done!
+echo 
 
+start %seleniumProjectPath%/testReports
 
 PAUSE
